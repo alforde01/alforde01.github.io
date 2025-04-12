09450dcd66c19f7d2856d070eaf20bb3fae74804
“alforde01“ <120608165+alforde01@users.noreply.github.com>
2025-04-11 21:38:43 -0400

    add navbar

---
+++ portfolio/src/components/Navbar.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <a href="#home">Home</a>
+        </li>
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#skills">Skills</a>
+        </li>
+        <li>
+          <a href="#projects">Work</a>
+        </li>
+        <li>
+          <a href="#contact">Education</a>
+        </li>
+        <li>
+          <a href="#extracurriculars">Extracurriculars</a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
--- portfolio/src/sections/Header.tsx
+++ portfolio/src/sections/Header.tsx
@@ -1,11 +1,22 @@
 import React from "react";
+import Navbar from "../components/Navbar";
 import "../styles/Header.css";
 
 const Header = () => {
   return (
     <section className="header">
-      <h1 className="header-title">Alissa</h1>
-      <p className="header-subtitle">Welcome!</p>
+      <div className="west-side">
+        <h1 className="header-title">Alissa Forde</h1>
+        <div className="header-subtitle marquee">
+          <div className="marquee-content">
+            Welcome! Feel free to look through my page and learn a little bit
+            more about me. I built this website to showcase my skills and
+            projects. First time using TypeScript and it was really fun! I hope
+            you enjoy your stay!
+          </div>
+        </div>
+      </div>
+      <Navbar />
     </section>
   );
 };
